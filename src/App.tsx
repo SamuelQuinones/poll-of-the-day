8a84145458cd422f824316480355ebe46bb31f2b
Samuel Quiñones <sdquinones1@gmail.com>
2021-11-12 13:01:24 -0500

    improved container class switching

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Header from "components/Header";
 import Container from "react-bootstrap/Container";
 import Footer from "components/Footer";
+import classNames from "classnames/bind";
 
 //* Views
 import PollSetup from "views/Setup";
@@ -11,20 +12,28 @@ import FinalResults from "views/Final";
 import { useAppSelector } from "store/hooks";
 import { getCurrentView } from "store/selectors";
 
+const viewClasses = {
+  poll: "justify-content-around",
+  setup: "justify-content-start",
+  end: "justify-content-between",
+};
+const cx = classNames.bind(viewClasses);
+
 function App() {
   //*REDUX
   const currentView = useAppSelector(getCurrentView);
 
+  const containerClasses = cx(
+    currentView,
+    "flex-column",
+    "d-flex",
+    "flex-fill"
+  );
+
   return (
     <div className="App">
       <Header />
-      <Container
-        as="main"
-        className={`flex-column d-flex flex-fill ${
-          currentView === "poll" && "justify-content-around"
-        } ${currentView === "setup" && "justify-content-start"}`}
-        fluid="md"
-      >
+      <Container as="main" className={containerClasses} fluid="md">
         {currentView === "setup" && <PollSetup />}
         {currentView === "poll" && <PollOfTheDay />}
         {currentView === "end" && <FinalResults />}
